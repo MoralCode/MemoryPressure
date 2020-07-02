d724708fb9fc6ae3d227807e483f7d4d1d4bbb65
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2020-07-02 14:58:58 -0700

    add getprocinfo subprocess to read values from /proc

--- mempressure-sensor.pl
+++ mempressure-sensor.pl
@@ -1,10 +1,49 @@
 #!/usr/bin/perl -w
 
+# use Data::Dumper qw(Dumper);
+
 $|=1;
 
 print "ksysguardd 1.2.0\n";
 print "ksysguardd> ";
 
+
+sub getprocinfo {
+    my ($file) = @_;
+    # arg1 = ; # file "memory, cpu, or io"
+    my $filepath = "/proc/pressure/${file}";
+
+    open(my $yourhandle, '<', $filepath) # always use a variable here containing filename
+    or die "Unable to open file, $!";
+
+
+    my %data = ();
+
+    while (my $line = <$yourhandle>) { # Read the file line per line (or otherwise, it's configurable).
+    chomp($line);
+
+    # print "in: ${line}-\n";
+    my @in = split(/\s+/, $line);
+    # print Dumper \%line;
+    
+    my $type = $in[0];
+    splice(@in, 0, 1);
+    # print "split: @in\n";
+
+    foreach $prop (@in) {
+    # print "iter: $prop\n";
+    ($key, @value) = split /=/, $prop;
+    # print "$type, $key = @value\n";
+    $data{$type}{$key} = "@value";
+    
+    }
+    }
+    close($yourhandle)
+    or warn "Unable to close the file handle: $!";
+
+    # print Dumper \%data;
+    return %data;
+}
 
 while(<>){
     if(/monitors/){
